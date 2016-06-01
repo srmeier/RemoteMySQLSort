ca6d4d681585359477d4b8c54adcdf08b1fa2a55
srmeier <info@stephenmeier.net>
2016-06-01 07:53:24 -0400

    setup sort item structure

--- RemoteMySQLSort/Main.cpp
+++ RemoteMySQLSort/Main.cpp
@@ -8,10 +8,16 @@
 #include "mysql/mysql.h"
 
 //-----------------------------------------------------------------------------
+void insert_sort(MYSQL_ROW* rows, int n,
+	int (*cmp)(const MYSQL_ROW* a, const MYSQL_ROW* b)
+) {
+
+}
+
+//-----------------------------------------------------------------------------
 #define H(key) hash_table[hash_multiplicative(key)]
 
 #define HASH_SIZE 1000
-
 int hash_table[HASH_SIZE];
 
 unsigned int hash_multiplicative(const char* key) {
@@ -22,7 +28,7 @@ unsigned int hash_multiplicative(const char* key) {
 		hash = 31*hash + key[i];
 	}
 
-	return hash % HASH_SIZE;
+	return (hash % HASH_SIZE);
 }
 
 //-----------------------------------------------------------------------------
@@ -42,7 +48,7 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
-	if(mysql_query(&db_con, "SELECT * FROM employees LIMIT 10;")) {
+	if(mysql_query(&db_con, "SELECT * FROM employees LIMIT 5;")) {
 		fprintf(stderr, "ERROR: \"%s\"\n", mysql_error(&db_con));
 		system("pause");
 		return -1;
@@ -77,6 +83,7 @@ int main(int argc, char** argv) {
 
 	// NOTE: the index into the array to be sorted is "i" and the value is one of the fields
 	for(int i=0; i<num_rows; ++i) {
+		/*
 		printf("emp_no:     %s\n", rows[i][H("emp_no")]);
 		printf("birth_date: %s\n", rows[i][H("birth_date")]);
 		printf("first_name: %s\n", rows[i][H("first_name")]);
@@ -84,6 +91,18 @@ int main(int argc, char** argv) {
 		printf("gender:     %s\n", rows[i][H("gender")]);
 		printf("hire_date:  %s\n", rows[i][H("hire_date")]);
 		printf("\n");
+		*/
+
+		int b_year, b_month, b_day;
+		sscanf(rows[i][H("hire_date")], "%d-%d-%d", &b_year, &b_month, &b_day);
+
+		char tmp[9] = "";
+		sprintf(tmp, "%04d%02d%02d", b_year, b_month, b_day);
+		int key = atoi(tmp);
+
+		printf("%d\n", key);
+
+		
 	}
 
 	// TESTING
