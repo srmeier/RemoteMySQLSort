c2f8515d67f218396579d5fbad4aa0554476aec1
srmeier <info@stephenmeier.net>
2016-06-09 10:48:45 -0400

    added a median sort algorithm

--- RemoteMySQLSort/Main.cpp
+++ RemoteMySQLSort/Main.cpp
@@ -25,6 +25,28 @@ unsigned int hash_multiplicative(const char* key) {
 }
 
 //-----------------------------------------------------------------------------
+int cmp_hire_date(const MYSQL_ROW a, const MYSQL_ROW b) {
+	char tmp[9] = "";
+	int year, month, day;
+
+	sscanf(a[H("hire_date")], "%d-%d-%d", &year, &month, &day);
+	sprintf(tmp, "%04d%02d%02d", year, month, day);
+	int a_key = atoi(tmp);
+
+	sscanf(b[H("hire_date")], "%d-%d-%d", &year, &month, &day);
+	sprintf(tmp, "%04d%02d%02d", year, month, day);
+	int b_key = atoi(tmp);
+
+	return (a_key - b_key);
+}
+
+/* INSERTION SORT
+- best    O(n)
+- average O(n^2)
+- worst   O(n^2)
+*/
+// NOTE: best for arrays that are small or nearly sorted
+//-----------------------------------------------------------------------------
 void insert_sort(MYSQL_ROW* rows, int n,
 	int (*cmp)(const MYSQL_ROW a, const MYSQL_ROW b)
 ) {
@@ -41,20 +63,82 @@ void insert_sort(MYSQL_ROW* rows, int n,
 	}
 }
 
+// NOTE: linear time partition function returning index "store" such that
+// [left, store] is less than or equal to "pivot" and (store, right] is greater
+// than "pivot" (no sorting is being done here)
 //-----------------------------------------------------------------------------
-int cmp_hire_date(const MYSQL_ROW a, const MYSQL_ROW b) {
-	char tmp[9] = "";
-	int year, month, day;
+int _partition(MYSQL_ROW* rows, int(*cmp)(const MYSQL_ROW a, const MYSQL_ROW b),
+	int left, int right, int pivot_index
+) {
+	int idx, store;
+	MYSQL_ROW pivot = rows[pivot_index];
 
-	sscanf(a[H("hire_date")], "%d-%d-%d", &year, &month, &day);
-	sprintf(tmp, "%04d%02d%02d", year, month, day);
-	int a_key = atoi(tmp);
+	// move pivot to the end of the array
+	MYSQL_ROW tmp = rows[right];
+	rows[right] = pivot;
+	rows[pivot_index] = tmp;
 
-	sscanf(b[H("hire_date")], "%d-%d-%d", &year, &month, &day);
-	sprintf(tmp, "%04d%02d%02d", year, month, day);
-	int b_key = atoi(tmp);
+	// all values <= pivot are moved to the front of array and pivot is
+	// inserted just after them
+	store = left;
+	for(idx = left; idx<right; ++idx) {
+		if(cmp(rows[idx], pivot) <= 0) {
+			tmp = rows[idx];
+			rows[idx] = rows[store];
+			rows[store] = tmp;
+			store++;
+		}
+	}
 
-	return (a_key > b_key);
+	tmp = rows[right];
+	rows[right] = rows[store];
+	rows[store] = tmp;
+
+	return store;
+}
+
+// NOTE: average-time linear (worst-case O(n^2)) function to find the position of
+// the kth element in a subarray [left, right]
+//-----------------------------------------------------------------------------
+int _select_kth(MYSQL_ROW* rows, int(*cmp)(const MYSQL_ROW a, const MYSQL_ROW b),
+	int k, int left, int right
+) {
+	// use a random first guess
+	int idx = rand()%((right+1)-left)+left;
+	int pivot_index = _partition(rows, cmp_hire_date, left, right, idx);
+
+	if(left+k-1 == pivot_index) { return pivot_index; }
+
+	// continue the loop, narrowing the range as appropriate. if we are within
+	// the left-hand side of the pivot then k can stay the same
+	if(left+k-1 < pivot_index)
+		return _select_kth(rows, cmp, k, left, pivot_index-1);
+	else
+		return _select_kth(rows, cmp, k-(pivot_index-left+1), pivot_index+1, right);
+}
+
+/* MEDIAN SORT
+- best    O(n*log(n))
+- average O(n*log(n))
+- worst   O(n^2)
+*/
+//-----------------------------------------------------------------------------
+void _median_sort(MYSQL_ROW* rows, int(*cmp)(const MYSQL_ROW a, const MYSQL_ROW b),
+	int left, int right
+) {
+	if(right <= left) return;
+
+	int mid = (right-left+1)/2;
+	int me  = _select_kth(rows, cmp, mid+1, left, right);
+
+	_median_sort(rows, cmp, left, left+mid-1);
+	_median_sort(rows, cmp, left+mid+1, right);
+}
+
+void median_sort(MYSQL_ROW* rows, int n,
+	int(*cmp)(const MYSQL_ROW a, const MYSQL_ROW b)
+) {
+	_median_sort(rows, cmp, 0, n-1);
 }
 
 //-----------------------------------------------------------------------------
@@ -107,7 +191,8 @@ int main(int argc, char** argv) {
 
 	// TESTING
 
-	insert_sort(rows, num_rows, cmp_hire_date);
+	//insert_sort(rows, num_rows, cmp_hire_date);
+	median_sort(rows, num_rows, cmp_hire_date);
 
 	// NOTE: the index into the array to be sorted is "i" and the value is one of the fields
 	for(int i=0; i<num_rows; ++i) {
